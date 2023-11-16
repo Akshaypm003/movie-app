73be9b3c8be4373cc6ac33a35ff363c0ef65c133
Akshay <apm@gmail.com>
2023-11-16 13:33:29 +0530

    mobile responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,7 @@ import './Navbar.css'
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-
-  const genreItem = "ps-5 text-base font-medium tracking-tight"; 
+ 
   const navigate = useNavigate();
 
   return (
@@ -15,20 +14,20 @@ function Navbar() {
       >
         NOVA.
       </h2>
-      <div className="nav-genre pe-10 my-auto ">
-        <span onClick={() => navigate("/trending")} className={genreItem}>
+      <div className="nav-genre flex flex-wrap pe-10 my-auto ">
+        <span onClick={() => navigate("/trending")} className='ps-5 text-base font-medium tracking-tight'>
           TRENDING
         </span>
-        <span onClick={() => navigate("/comedy")} className={genreItem}>
+        <span onClick={() => navigate("/comedy")} className='ps-5 text-base font-medium tracking-tight'>
           COMEDY
         </span>
-        <span onClick={() => navigate("/action")} className={genreItem}>
+        <span onClick={() => navigate("/action")} className='ps-5 text-base font-medium tracking-tight'>
           ACTION
         </span>
-        <span onClick={() => navigate("/horror")} className={genreItem}>
+        <span onClick={() => navigate("/horror")} className='ps-5 text-base font-medium tracking-tight'>
           HORROR
         </span>
-        <span onClick={() => navigate("/romance")} className={genreItem}>
+        <span onClick={() => navigate("/romance")} className='ps-5 text-base font-medium tracking-tight'>
           ROMANCE
         </span>
       </div>
